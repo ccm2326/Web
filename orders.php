5c89a79c80378d83508528b59b2db52de8dff4ae
camtcm <camila.carpio.mollo@ucsp.edu.pe>
2024-10-06 13:52:32 -0500

    Información en Pedidos

--- orders.php
+++ orders.php
@@ -55,15 +55,15 @@ if(isset($_SESSION['user_id'])){
             while($fetch_orders = $select_orders->fetch(PDO::FETCH_ASSOC)){
    ?>
    <div class="box">
-      <p>placed on : <span><?= $fetch_orders['placed_on']; ?></span></p>
-      <p>name : <span><?= $fetch_orders['name']; ?></span></p>
-      <p>email : <span><?= $fetch_orders['email']; ?></span></p>
-      <p>number : <span><?= $fetch_orders['number']; ?></span></p>
-      <p>address : <span><?= $fetch_orders['address']; ?></span></p>
-      <p>payment method : <span><?= $fetch_orders['method']; ?></span></p>
-      <p>your orders : <span><?= $fetch_orders['total_products']; ?></span></p>
-      <p>total price : <span>$<?= $fetch_orders['total_price']; ?>/-</span></p>
-      <p> payment status : <span style="color:<?php if($fetch_orders['payment_status'] == 'pending'){ echo 'red'; }else{ echo 'green'; }; ?>"><?= $fetch_orders['payment_status']; ?></span> </p>
+      <p>Fecha de pedido: <span><?= $fetch_orders['placed_on']; ?></span></p>
+      <p>Nombre: <span><?= $fetch_orders['name']; ?></span></p>
+      <p>Email: <span><?= $fetch_orders['email']; ?></span></p>
+      <p>Número: <span><?= $fetch_orders['number']; ?></span></p>
+      <p>Dirección: <span><?= $fetch_orders['address']; ?></span></p>
+      <p>Método de pago: <span><?= $fetch_orders['method']; ?></span></p>
+      <p>Tus pedidos: <span><?= $fetch_orders['total_products']; ?></span></p>
+      <p>Precio total: <span>$<?= $fetch_orders['total_price']; ?>/-</span></p>
+      <p>Estado de pago: <span style="color:<?php if($fetch_orders['payment_status'] == 'pending'){ echo 'red'; }else{ echo 'green'; }; ?>"><?= $fetch_orders['payment_status']; ?></span> </p>
    </div>
    <?php
       }
